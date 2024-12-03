087fea1bf8379ff6b86c7eb4284dbaaebda49dce
camiamibeisser <camilaferraz.beisser@gmail.com>
2024-12-03 11:32:27 -0300

    Add files via upload
    03/12/2024

---
+++ aula 2/aula02.py
@@ -0,0 +1,7 @@
+print("ola mundo")
+print("meu nome é Camila")
+nome_usuario='Camila' # tipo -> str = string
+idade_usuario= 21 # tipo -> int = integer
+altura_usuario= 1.48 # tipo -> float = ponto flutuante
+maior_idade= True # tipo -> bool = boolean
+print("seja bem vinda", nome_usuario ,"sua idade é",idade_usuario ,"anos, você tem" ,altura_usuario ,"metros. Você é maior de idade?" ,maior_idade)
